136ab8b8d3983dba67fd07b94485cce297a7dbf3
maisha850 <tasnimmaisha08@gmail.com>
2025-08-25 20:51:55 +0600

    added to the final one

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ document.getElementById('btn').addEventListener('click',function(event){
     const mobile=getvalueFrominputfield('input-num')
     const pin=getvalueFrominputfield('input-pin')
     
-    
-    if(mobile===12345678910 && pin===1234){
+    if(isNaN(mobile)){
+        return 'Invalid Number'
+    }
+    if(mobile.length===11 && pin===1234){
         window.location.href='./home.html'
     }
     else{
